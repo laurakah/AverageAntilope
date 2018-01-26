eab53ea68921faa8b550479f24f33660a4eec9bb
Luci <mick@d17.de>
2018-01-26 17:31:39 +0100

    initial commit

---
+++ averageData.py
@@ -0,0 +1,86 @@
+import json
+import copy
+import time
+
+class AverageData():
+
+    OUTPUT_FILE = "averageData.txt"
+    INPUT_FILE = "testData.json"
+
+    def __init__(self):
+        self.rawDataJson = []
+        self.locationData = []
+        self.shortenedLocations = []
+
+
+    def _createSingleLocationPoint(self, dataSet):
+        singleLocation = []
+        singleLocation.append(str(dataSet["location"]["coordinates"][0])[:5])
+        singleLocation.append(str(dataSet["location"]["coordinates"][1])[:5])
+        return singleLocation
+
+    def _compareLocations(self, dataSet, shortenedLocations):
+        shortLoc = self._createSingleLocationPoint(dataSet)
+        if shortLoc in shortenedLocations:
+            return True
+        else:
+            return False
+
+    def _findIndexByLocation(self, dataSet, locationData):
+        for i in range(len(locationData)):
+            if self._createSingleLocationPoint(dataSet) != self._createSingleLocationPoint(locationData[i]):
+                continue
+            return i
+
+    def _countDoubleLocations(self, dataList, shortenedLocations):
+        for dataSet in dataList:
+            singleLocationData = {}
+            exists = self._compareLocations(dataSet, shortenedLocations)
+            if exists == False:
+                shortLoc = self._createSingleLocationPoint(dataSet)
+                shortenedLocations.append(shortLoc)
+                singleLocationPoint = copy.deepcopy(dataSet)
+                singleLocationPoint.update({"count": 1})
+                self.locationData.append(singleLocationPoint)
+            else:
+                i = self._findIndexByLocation(dataSet, self.locationData)
+                self.locationData[i]["count"] += 1
+                k = dataSet["deviceData"].keys()
+                for key in k:
+                    if key in ["time"]:
+                        continue
+                    self.locationData[i]["deviceData"][key] += dataSet["deviceData"][key]
+
+    def _calculateAverageData(self, dataList):
+        if len(dataList) == 0:
+            return
+        data = copy.deepcopy(dataList)
+        k = data[0]["deviceData"].keys()
+        for i in range(len(data)):
+            for key in k:
+                if key in ["time"]:
+                    continue
+                data[i]["deviceData"][key] /= data[i]["count"]
+        return data
+
+    def _writeDataToFile(self, data, fileName):
+        f = open(fileName, "w")
+        f.write(json.dumps(data))
+        f.close()
+
+    def run(self):
+        self.rawDataJson = json.load(open(self.INPUT_FILE))
+        self._countDoubleLocations(self.rawDataJson, self.shortenedLocations)
+        data = self._calculateAverageData(self.locationData)
+        self._writeDataToFile(data, self.OUTPUT_FILE)
+        return data
+
+def main():
+    # 22.5 s run time / 100.000 data sets
+    print time.time()
+    a = AverageData()
+    a.run()
+    print time.time()
+
+if __name__ == "__main__":
+    main()
---
+++ data.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+import json
+import random
+import time
+import copy
+
+LONGITUDE_CONST = 520000.0
+LONGITUDE_MIN = 2240
+LONGITUDE_MAX = 3811
+LATITUDE_CONST = 130000.0
+LATITUDE_MIN = 2933
+LATITUDE_MAX = 9000
+SOUND_MIN = 0
+SOUND_MAX = 800
+LIGHT_MIN = 0
+LIGHT_MAX = 600
+DUST_MIN = 0
+DUST_MAX = 8000
+UV_MIN = 0
+UV_MAX = 400
+ACCELERATION_MIN = 0
+ACCELERATION_MAX = 500
+TEMPERATURE_MIN = -20
+TEMPERATURE_MAX = 55
+HUMIDITY_MIN = 0
+HUMIDITY_MAX = 100
+
+def generateData(key, min, max, dic):
+    dataValue = random.randint(min, max)
+    dataKey = key
+    dic[dataKey] = dataValue
+
+def generateLocation(min, max, const):
+    dataValue = (const + random.randint(min, max)) / 10000.0
+    return dataValue
+
+def generateTime(dic):
+    dataValue = time.strftime("%H:%M:%S")
+    dataKey = "time"
+    dic[dataKey] = dataValue
+
+def generateId(buff):
+    dataValue = random.randint(0, 10000)
+    dataKey = "deviceId"
+    buff[dataKey] = dataValue
+
+def wrapData(buff, dic):
+    generateId(buff)
+    dataValue = dic
+    dataKey = "deviceData"
+    buff[dataKey] = dataValue
+
+def wrapLocation(buff):
+    geoJsonWrapper = {"type": "Point"}
+    coordinates = []
+    coordinateKey = "coordinates"
+    coordinates.append(generateLocation(LONGITUDE_MIN, LONGITUDE_MAX, LONGITUDE_CONST))
+    coordinates.append(generateLocation(LATITUDE_MIN, LATITUDE_MAX, LATITUDE_CONST))
+    geoJsonWrapper[coordinateKey] = coordinates
+    dataKey = "location"
+    buff[dataKey] = geoJsonWrapper
+
+def writeData(data, fileName):
+    json.dump(data, fileName)
+
+def generateJson(dataSource, locationSource, dic):
+    generateTime(dic)
+    for keyName in dataSource.keys():
+        generateData(keyName, dataSource[keyName][0], dataSource[keyName][1], dic)
+
+def generateDataSet(dataSource, locationSource, dic, buff):
+    generateJson(dataSource, locationSource, dic)
+    wrapData(buff, dic)
+    wrapLocation(buff)
+    return buff
+
+def main():
+    dataFile = open("testData.json", "a")
+    sensorData = {"noise": [SOUND_MIN, SOUND_MAX], "light": [LIGHT_MIN, LIGHT_MAX], "pollution": [DUST_MIN, DUST_MAX], "uv": [UV_MIN, UV_MAX], "acceleration": [ACCELERATION_MIN, ACCELERATION_MAX], "temperature": [TEMPERATURE_MIN, TEMPERATURE_MAX], "humidity": [HUMIDITY_MIN, HUMIDITY_MAX]}
+    locationData = {"longitude": [LONGITUDE_MIN, LONGITUDE_MAX, LONGITUDE_CONST], "latitude": [LATITUDE_MIN, LATITUDE_MAX, LATITUDE_CONST]}
+
+    dataList = []
+    jsonBuffer = {}
+    jsonData = {}
+    for i in range(1, 1000):
+        buff = generateDataSet(sensorData, locationData, jsonData, jsonBuffer)
+        dataList.append(copy.deepcopy(buff))
+    writeData(dataList, dataFile)
+
+main()
